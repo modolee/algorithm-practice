ea9d6febde0670c89ebcdf8ff5ed5796243c8a4f
modolee <lsi1318@gmail.com>
2018-04-04 22:24:54 +0900

    added a more efficient solution for most-numbers.

--- checkio-python/elementary/most-numbers.py
+++ checkio-python/elementary/most-numbers.py
@@ -1,8 +1,5 @@
 def checkio(*args):
-    if (len(args) == 0):
+    if(len(args) == 0) :
         return 0
-
-    arg_list = list(args)
-    arg_list.sort()
-
-    return float(arg_list[len(arg_list) - 1]) - float(arg_list[0])
+    else :
+        return max(args) - min(args)
