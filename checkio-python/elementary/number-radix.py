c1750b84560ba2c1f364eab5a55a05b9c09ce5b0
modolee <lsi1318@gmail.com>
2018-04-16 22:52:12 +0900

    number-radix 2nd trial

--- checkio-python/elementary/number-radix.py
+++ checkio-python/elementary/number-radix.py
@@ -1,12 +1,3 @@
 def checkio(str_number, radix):
-    number_list = list(map(lambda x: int(x) if ord(x) < ord('A') else int(ord(x) - ord('A') + 10), str_number))
-
-    if list(filter(lambda x: x >= radix, number_list)):
-        return -1
-
-    sum = 0;
-    radix_index = len(number_list) - 1
-    for i in range(0, len(number_list)):
-        sum += number_list[i] * radix ** (radix_index - i)
-
-    return sum
+    try : return int(str_number, radix)
+    except : return -1
