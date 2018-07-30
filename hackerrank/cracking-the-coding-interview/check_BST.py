bb81b287ef4a667fc41501a2d9ac64d9e79a7b50
modolee <lsi1318@gmail.com>
2018-07-30 15:04:42 +0900

    효율적인 Binary Search Tree 유효성 검사 방법으로 수정

--- hackerrank/cracking-the-coding-interview/check_BST.py
+++ hackerrank/cracking-the-coding-interview/check_BST.py
@@ -6,67 +6,14 @@ class node:
         self.right = None
 """
 
+def recursiveBST(root, min, max):
+    if root == None:
+        return True
+    if root.data <= min or root.data >= max:
+        return False
+    else:
+        return recursiveBST(root.left, min, root.data) and recursiveBST(root.right, root.data, max)
+
 
 def checkBST(root):
-    left = root.left
-    right = root.right
-    checkLeft = False
-    checkRight = False
-    leftChildren = []
-    rightChildren = []
-
-    if left == None:
-        checkLeft = True
-    elif left.data >= root.data:
-        return False
-    else:
-        leftChildren = childList(left)
-        for child in leftChildren:
-            if child >= root.data:
-                return False
-        checkLeft = checkBST(left)
-
-    if right == None:
-        checkRight = True
-    elif right.data <= root.data:
-        return False
-    else:
-        rightChildren = childList(right)
-        for child in rightChildren:
-            if child <= root.data:
-                return False
-        checkRight = checkBST(right)
-
-    childrenList = []
-    childrenList.append(root.data)
-    if leftChildren:
-        childrenList += leftChildren
-    if rightChildren:
-        childrenList += rightChildren
-
-    childrenSet = set(childrenList)
-
-    if len(childrenSet) == len(childrenList):
-        return checkLeft and checkRight
-    else:
-        return False
-
-
-def childList(root):
-    left = root.left
-    right = root.right
-    children = []
-
-    if left != None:
-        children.append(left.data)
-        leftChildren = childList(left)
-        if leftChildren:
-            children += leftChildren
-
-    if right != None:
-        children.append(right.data)
-        rightChildren = childList(right)
-        if rightChildren:
-            children += rightChildren
-
-    return children
+    return recursiveBST(root, -1, 10001)
